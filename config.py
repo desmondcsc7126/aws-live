9ebc9101d24f783ae538d516f2f740d1f7afe5c4
desmondcsc7126 <101800952+desmondcsc7126@users.noreply.github.com>
2022-03-17 18:19:07 +0800

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-customhost = "database-1.cpz7zg34rcbs.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "bait3273"
-customdb = "database-1"
-custombucket = "desmondchongsonchuen-employee"
-customregion = "us-east-1"
