4d52eeb2c88aa281c373b45af4fe29e2e2769c72
desmondcsc7126 <101800952+desmondcsc7126@users.noreply.github.com>
2022-03-17 18:11:58 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cpz7zg34rcbs.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bait3273"
+customdb = "database-1"
+custombucket = "desmondchongsonchuen-employee"
+customregion = "us-east-1"
