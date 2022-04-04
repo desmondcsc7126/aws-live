381a2890e05e62c1dc8d72c3904aafdb6a84b8a0
desmondcsc7126 <101800952+desmondcsc7126@users.noreply.github.com>
2022-04-04 21:01:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ customuser = "aws_user"
 custompass = "dcsc12345"
 customdb = "employee"
 
-access_key = 'ASIA23IK2VOCHPG7EUM7'
-secret_access_key = 'Ttk8o38AQQ5vaIpXkIEYlUkJAuCnPuotfdq9ngSv'
-session_token = 'FwoGZXIvYXdzEL3//////////wEaDNIWN6vNQ4XisHo0OyLUARdxE2NKY5Dlgv7Q2BdTgKlp7MwfGe6qfOes5nNHnfp6X6BYX6wnXlLnT3xXC6GVFZwmp4x+RTAX7VCfbBeNkztjUQsw9DkRg3mzQuqaj4GEGoKwrJVv9mpt+mgeKlbw3MCpTcIhWIssLFQkwY8DH9taWPpHH8bt8du5zfumNdUw7OGQayCrvdXZjE2mc2rcNXRBp1xJ7owxHrqhXpujxvBxJMK41F1QF7hrVR+kVmTMwmqoumXtZfjeK3Ta9hDh2kvm+vE2bnH0lWbgHIxHsvV3c9vsKIXloJIGMi3zgcU55/k7vSfdp7XiesWMmLxKICxyOoQPXqtsxXPduPYpQ4zHpscYtYQwNN4='
+access_key = 'ASIA23IK2VOCEWZ4F4KY'
+secret_access_key = '6Yhp5LK3PQi7i++57OQZT8S0W0ame+TfGJQm2ll+'
+session_token = 'FwoGZXIvYXdzEO7//////////wEaDGKlBJ/ydmFxh/ohyCLUAbFwRW3mzkyNFyykasj9bO9vGp1M5iLP/b/IpPyG97Mk/1D2oQCPFRB/jKoN83Ws23IkpkVErPr+n4DqclehHuhMwhocAD8Ua6S1cSy2+TmAJKEnpkVfm1D3Og0nFBPPfQZk0s+4Zfr/HAqPSvMhsyOatjbkoSY/4Gcax27XXHL1hJUOaxoBLgt6tLHWEWOsP+lYXu5+EFaaO/inXS5evn/8xKlqKyx25JucHlO+d1KgZYtdjHkmHPuNS/LCy8BL5OYdILepEHdTrinLMlWNuGQgk9x+KM/Wq5IGMi1A2zJaLar9LzgrG9hHVU/pqA9+goNNKcgOsz32Rkq8vvUGWk8OU1afDhhB90Q='
